437c9dd743b8ad3db657ae6696f21235399c5fcd
五丝菜卷 <chenjiegd@foxmail.com>
2019-05-13 16:58:10 +0800

    更新

--- 第二章程序练习题/汉诺塔v1.py
+++ 第二章程序练习题/汉诺塔v1.py
@@ -1,14 +1,18 @@
 i = 0
-def hanoi(n,x,y,z):
-        global i
-        if n == 1:
-                i += 1
-                print(i,'...',x,'-->',z)
-        else:
-                hanoi(n-1,x,z,y)
-                i += 1
-                print(i,'...',x,'-->',z)
-                hanoi(n-1,y,x,z)
-n = 19
-hanoi(n,'X','Y','Z')
-print('总共需要{}步。'.format(i))  ##  i = 2 ** n - 1
+
+
+def hanoi(n, x, y, z):
+    global i
+    if n == 1:
+        i += 1
+        print(i, '...', x, '-->', z)
+    else:
+        hanoi(n - 1, x, z, y)
+        i += 1
+        print(i, '...', x, '-->', z)
+        hanoi(n - 1, y, x, z)
+
+
+n = 20
+hanoi(n, 'X', 'Y', 'Z')
+print('总共需要{}步。'.format(i))  # i = 2 ** n - 1
